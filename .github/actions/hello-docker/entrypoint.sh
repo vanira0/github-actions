d74d3e4adbf73c9c6f65fd6ffec2fa9b50c5fbbc
Vani Rao <vanirao322@gmail.com>
2025-08-02 18:37:42 +0530

    fix entrypoint

--- .github/actions/hello-docker/entrypoint.sh
+++ .github/actions/hello-docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo"::debug::Running entrypoint.sh"
 echo "Hello, $1!"
-echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET
+echo "INPUT_WHO_TO_GREET: $INPUT_WHO_TO_GREET"
 echo "HELLO: $HELLO"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
